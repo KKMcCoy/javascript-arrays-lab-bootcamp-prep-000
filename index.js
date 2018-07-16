b5351db0f8aa07226781ec368384a4fb8cd5cfeb
karen tsui <buzz.kyt@gmail.com>
2018-07-16 15:17:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var kittens = ["Milo", "Otis", "Garfield"]// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+Function destructivelyAppendKitten(name) {
+
+}
+
+
+
+// Add your functions and code here
