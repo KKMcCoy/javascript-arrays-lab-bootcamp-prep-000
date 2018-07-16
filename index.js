1327e2d40b2a6c0a628158ad4317ec37068e39fa
karen tsui <buzz.kyt@gmail.com>
2018-07-16 15:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
- kittens.push(name);
- return kittens;
+function destructivelyAppendKitten(name) {;
+ return [...kittens, "name"];
  }
 
 
